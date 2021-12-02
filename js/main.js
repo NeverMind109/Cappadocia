f815caa53cb0e5ac530f20ddc061e7745282895d
NeverMind109 <vladgladkih123@gmail.com>
2021-12-02 21:33:21 +0300

    Created: Added Fairy-tail Section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-
+    $('.fairy-tail__slider').slick({
+        infinite: true,
+        nextArrow: '<button type = "button" class = "slick-btn slick-next"><img src="images/next-arrow.svg" alt="Icon: Next"></ button>',
+        prevArrow: '<button type = "button" class = "slick-btn slick-prev"><img src="images/prev-arrow.svg" alt="Icon: Prev"></ button>',
+        autoplay: true,
+        fade: true
+    });
 });
